fa6fce7f3a6e3e03f755c00e09c689b92a364b34
Muhammet Madraimov <muhammetmadraimov@Muhammets-MacBook-Pro.local>
2018-06-25 14:24:37 +0300

    Initial commit

---
+++ azuredeploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# login to customer account
+az login
+
+LOCATION="WestEurope"
+RESGROUPNAME="from-market3"
+
+# Select subscription name
+SUBSCRIPTION="Production"
+
+az account set -s $SUBSCRIPTION
+
+# create resource group
+echo "Creating resource group..."
+az group create -l $LOCATION -n $RESGROUPNAME
+
+
+# deploy VM
+echo "Start deployment..."
+startime=`date +%H:%M:%S`
+
+az group deployment create \
+    --name "MXV-deployment" \
+    --resource-group $RESGROUPNAME \
+    --template-file template.json \
+    --parameters \
+                location=$LOCATION 
+
+echo "Finish deployment..." 
+echo $startime"---"`date +%H:%M:%S`
+
